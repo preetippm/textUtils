c17a57916f095975ca6d9351d545b16f03e37800
Preeti Mallick <preetimallick@Preetis-MacBook-Pro.local>
2021-10-09 17:29:03 +0530

    changed the project name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">My App</a>
+    <a className="navbar-brand" href="/">textUtils</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
